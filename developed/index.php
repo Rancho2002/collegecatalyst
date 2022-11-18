e91e70187c1ef521dfdd15a83a77040c758faf7e
Arijit <subhasghosh196@outlook.com>
2022-11-18 17:11:07 +0530

    isolated footer

--- developed/index.php
+++ developed/index.php
@@ -304,60 +304,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </a>
       </div>
 
-      <div>
-        <form action="" method="post">
-          <div class="gap-4 justify-center items-center">
-            <div class="md:ml-auto md:mb-6">
-              <p class="" id="contact">
-                <strong>Send a message to our team</strong>
-              </p>
-            </div>
-
-            <div class="md:mb-6 w-full md:w-1/2 mx-auto">
-              <input type="email" class="
-                  form-control
-                  block
-                  w-full
-                  px-3
-                  py-1.5
-                  text-base
-                  font-normal
-                  text-gray-700
-                  bg-white bg-clip-padding
-                  border border-solid border-gray-300
-                  rounded
-                  transition
-                  ease-in-out
-                  m-0 mb-2
-                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                " id="exampleFormControlInput1" placeholder="Enter your email address" name="email" required/>
-              <input type="text" class="
-                  form-control
-                  block
-                  w-full
-                  px-3
-                  py-1.5
-                  text-base
-                  font-normal
-                  text-gray-700
-                  bg-white bg-clip-padding
-                  border border-solid border-gray-300
-                  rounded
-                  transition
-                  ease-in-out
-                  m-0 mb-3
-                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                " id="exampleFormControlInput1" placeholder="type your message" name="message" />
-            </div>
-
-            <div class="md:mr-auto mb-6">
-              <button type="submit" class="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
-                Mail Us
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+      <?php include "./parts/footer.php"; ?>
 
       <div class="mb-6">
         <p>
